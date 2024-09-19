7bb31a574f5806c101f18892fe94b81ea31318a7
andrewcresciullo <andrewcresciullo@localhost>
2024-09-19 11:16:04 -0500

    hey

---
+++ src/studio3/Studio3.java
@@ -0,0 +1,21 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Studio3  {
+	public static void main(String[] args) {
+Scanner in = new Scanner(System.in);{
+	int lowerbound=2;
+	System.out.println("Please select the upper bound for your range");
+	int UpperBound=in.nextInt();
+	
+	System.out.println("Your range is:  " + lowerbound + " to " + UpperBound);
+	
+
+	boolean[]a = new boolean[UpperBound];
+	System.out.println(a[UpperBound-2]);
+	
+	
+	
+}}}
+
