87f10e012c8dfca91af21d0f44e609790c81bb83
andrewcresciullo <andrewcresciullo@localhost>
2024-09-19 11:16:19 -0500

    hi

--- src/studio3/Studio3.java
+++ src/studio3/Studio3.java
@@ -4,18 +4,26 @@ import java.util.Scanner;
 
 public class Studio3  {
 	public static void main(String[] args) {
-Scanner in = new Scanner(System.in);{
-	int lowerbound=2;
-	System.out.println("Please select the upper bound for your range");
-	int UpperBound=in.nextInt();
-	
-	System.out.println("Your range is:  " + lowerbound + " to " + UpperBound);
-	
+		Scanner in = new Scanner(System.in);{
+			int lowerbound=2;
+			System.out.println("Please select the upper bound for your range");
+			int UpperBound=in.nextInt();
 
-	boolean[]a = new boolean[UpperBound];
-	System.out.println(a[UpperBound-2]);
-	
-	
-	
-}}}
+			System.out.println("Your range is:  " + lowerbound + " to " + UpperBound);
+
+
+			boolean[]a = new boolean[UpperBound];
+			System.out.println(a[UpperBound-2]);
+			for (int i = 0; i < UpperBound+1; i++) {
+				a[i] = true; 
+				int b = i++; 
+			for (int j = (i^2)+(b)){
+			
+
+			}
+
+
+
+
+		}}}
 
